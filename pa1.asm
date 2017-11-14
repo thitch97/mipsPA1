831a961fb90df44a7c13f28cb60245b976f7d4d5
Tim <th.einstein97@gmail.com>
2017-11-13 23:53:32 -0500

    Declare and allocate space for input data and prompts

--- pa1.asm
+++ pa1.asm
@@ -1,5 +1,13 @@
- # Timothy Hitchener 
- #Programming Assignment 1
- 
- #Take in input
- 
+#--------------------------------------------------
+# Timothy Hitchener 
+# Programming Assignment 1
+
+.data
+	input:	.space 9 		# Space allocated for 8 characters and a terminating character
+	prompt:	.asciiz "Enter a hexadecimal number:"	
+	error: 	.asciiz "Hexadecimal number is invalid."
+
+.text
+
+main:
+	
