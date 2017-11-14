8207d0fc1f2f26694c6ab39becfdda2706ebb5d8
Tim <th.einstein97@gmail.com>
2017-11-14 00:52:57 -0500

    Implement main - prompt for and store user input

--- pa1.asm
+++ pa1.asm
@@ -1,6 +1,22 @@
 #--------------------------------------------------
 # Timothy Hitchener 
-# Programming Assignment 1
+# Programming Assignment 1 -- Reads a string of up to 8 characters from user input and checks its validity as 
+#			      a hexadecimal value and prints out the corresponding decimal value.
+
+# Registers:
+#	$t0 = 
+#	$t1 = 
+# Syscall parameters:	
+#	$v0 = 
+#	$a0 = 
+#
+#
+#
+#
+#
+#
+#
+
 
 .data
 	input:	.space 9 		# Space allocated for 8 characters and a terminating character
@@ -10,4 +26,22 @@
 .text
 
 main:
+
+	la $a0, prompt		# load the address of the prompt into the syscall parameter
+	li $v0, 4		# load the syscall for printing a string into syscall variable
+	syscall
 	
+	la $a0, input		# load the input into the syscall parameter
+	li $a1, 9		# allocate the space for the input string	
+	li $v0, 8		# load the syscall for read string
+	syscall
+	
+	la $a0, ($t0)
+	
+	addi $t4, $zero, 0
+
+
+	
+	
+	
+
